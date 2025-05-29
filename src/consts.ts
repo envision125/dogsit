66498747e4819975ef16dcb9ae569ce7a9e5dfb1
envision125 <126027195+envision125@users.noreply.github.com>
2025-05-28 17:54:17 -0700

    update background

---
+++ src/consts.ts
@@ -0,0 +1,14 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';
+
+
+
+
+// METATAGS for readers, for social media links
+export const SC_FB_TAG = 'Tortas Ahogadas Mi barrio';
+export const SC_IG_TAG = 'Tortas Ahogadas Mi barrio';
+export const SC_TK_TAG = 'Tortas Ahogadas Mi barrio';
