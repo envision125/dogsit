a21c04cd29e0af8bd6d271826a1ba65aea960c4b
envision125 <126027195+envision125@users.noreply.github.com>
2025-07-16 00:22:13 -0700

    fix the footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
+export const SITE_TITLE = 'Sunnyside Day Care';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';
 
